3e1d151d23ce35e14610084dc98bec4549bf48ac
newwohh <nevoznvx@gmail.com>
2024-07-11 00:50:05 +0530

    fix: added types for frontend

--- depresso.ai/src/routes/+page.svelte
+++ depresso.ai/src/routes/+page.svelte
@@ -1,48 +1,56 @@
-
 <script lang="ts">
-    import { onMount } from 'svelte';
-  
-    let userInput = '';
-    let emotions: string[] = [];
-  
-    const analyzeEmotions = async () => {
-      const response = await fetch('http://localhost:3000/analyze', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ text: userInput })
-      });
-  
-      const data = await response.json();
-      const { positive, negative, neutral } = data;
-      emotions = [`Positive: ${positive}`, `Negative: ${negative}`, `Neutral: ${neutral}`];
-    };
-  
-    onMount(async () => {
-      // Placeholder for any initialization if needed
+  import { onMount } from "svelte";
+
+  type Body = {
+    positive: number;
+    negative: number;
+    neutral: number;
+  };
+
+  let userInput: string = "";
+  let emotions: string[] = [];
+
+  const analyzeEmotions = async () => {
+    const response: Response = await fetch("http://localhost:3000/analyze", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text: userInput }),
     });
-  </script>
-  
-  <main>
-    <textarea bind:value={userInput} placeholder="Enter text to analyze emotions"></textarea>
-    <button on:click={analyzeEmotions}>Analyze Emotions</button>
-  
-    <div>
 
-            <span>
-                {emotions.map(emotion => {emotion})}
-            </span>
-    </div>
-  </main>
+    const data: Body = await response.json();
+    const { positive, negative, neutral } = data;
+    emotions = [
+      `Positive: ${positive}`,
+      `Negative: ${negative}`,
+      `Neutral: ${neutral}`,
+    ];
+  };
 
+  onMount(async () => {
+    // Placeholder for any initialization if needed
+  });
+</script>
 
-  
-  <style>
-    textarea {
-      width: 100%;
-      height: 100px;
-      margin-bottom: 10px;
-    }
-  </style>
-  
+<main>
+  <textarea bind:value={userInput} placeholder="Enter text to analyze emotions"
+  ></textarea>
+  <button on:click={analyzeEmotions}>Analyze Emotions</button>
+
+  <div>
+    <span>
+      {emotions.map((emotion) => {
+        emotion;
+      })}
+    </span>
+  </div>
+</main>
+
+<style>
+  textarea {
+    width: 100%;
+    height: 100px;
+    margin-bottom: 10px;
+  }
+</style>
